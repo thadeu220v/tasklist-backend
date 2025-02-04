c9aec14c7bf18a4f4cb30a5172e4be7d46a03aa5
thadeu220v <ardennto@gmail.com>
2025-02-04 14:38:24 -0300

    Adicionado o método post para atualizações parciais de dados

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ app.get('/tasks/:id', (req, res) => {
 app.put('/tasks/:id', (req, res) => {
     let task = tasksToDo.find(tarefa => tarefa.id === parseInt(req.params.id));
     if(!task) {
-        res.status(404).send('Não encontramos esta tarefa para edição');
+        res.status(404).send('Não encontramos esta tarefa para atualização');
     }
 
 let body = req.body;
@@ -74,6 +74,42 @@ if (body.status !== undefined) task.status = body.status;
 
 res.status(200).json(task);
 });
+
+// rota para atualização parcial patch dos campos da tarefa
+app.patch('/tasks/:id', (req, res) => {
+    let task = tasksToDo.find(tarefa => tarefa.id === parseInt(req.params.id));
+    if(!task) {
+        res.status(404).send('Não encontramos esta tarefa para atualização');
+    }
+
+let body = req.body;
+
+if (body.title && body.title.length <= 3) {
+    return res.status(400).send('Titulo obrigatório deve ser maior que 3 caracteres');
+}
+
+
+if (body.status !== undefined && typeof body.status !== 'boolean') {
+    return res.status(400).send('O status deve ser true ou false, do tipo boolean.');
+}
+
+if (body.title) task.title = body.title;
+if (body.description) task.description = body.description;
+if (body.status !== undefined) task.status = body.status;
+
+res.status(200).json(task);
+});
+
+app.delete('/tasks/:id', (req, res) => {
+    let task = tasksToDo.find(tarefa => tarefa.id === parseInt(req.params.id));
+    let taskIndex = tasksToDo.findIndex(tarefa => tarefa.id === task);
+    if(!task ===-1) {
+        res.status(404).send('a tarefa não pode ser deletada porque ela não existe');
+    }
+
+    tasksToDo.splice(taskIndex, 1);
+    res.status(200).send('Tarefa deletada com sucesso');
+})
 
 app.listen(3000, () => {
     console.log('o servidor está funcionando na porta 3000');
